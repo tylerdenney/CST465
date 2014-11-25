9f88bf2116effe71082e5d57df09af2649c28ad0
tylerdenney <tjay930@hotmail.com>
2014-11-24 23:15:04 -0800

    added profilepersistance class

--- MyWebDevProject/Account/EditProfile.aspx.cs
+++ MyWebDevProject/Account/EditProfile.aspx.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using MyWebDevProject.Old_App_Code;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -17,7 +21,9 @@ public partial class EditProfile : System.Web.UI.Page
         if (Page.IsValid)
         {
             pdata = new ProfileData(uxUserType.Text, uxName.Text, uxHobby.Text, uxBand.Text, uxBiography.Text, uxCoursePrefix.Text, uxCourseNumber.Text, uxCourseDescription.Text);
-            Session["ProfileData"] = pdata;
+            ProfilePersistance pp = new ProfilePersistance();
+            pp.SaveProfile(pdata);
+           
             Response.Redirect("ViewProfile.aspx");
         }
     }
--- MyWebDevProject/Account/ViewProfile.aspx.cs
+++ MyWebDevProject/Account/ViewProfile.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyWebDevProject.Old_App_Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,8 @@ public partial class ViewProfile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        ProfileData pdata = (ProfileData)Session["ProfileData"];
+        ProfilePersistance pp = new ProfilePersistance();
+        ProfileData pdata = pp.LoadProfile();
         uxName.Text = pdata.Name;
         uxHobby.Text = pdata.Hobby;
         uxBand.Text = pdata.Band;
